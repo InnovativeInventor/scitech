66bcb8d40919808f8479717e71098ea8cd3f1ab1
InnovativeInventor <theinnovativeinventor@gmail.com>
2019-03-23 23:41:35 -0400

    Updated logo

--- optimize.py
+++ optimize.py
@@ -9,7 +9,7 @@ import tqdm
 
 ORIGINAL_DIR = '/Users/max/git/scitech/docs-temp/'
 OUTPUT_DIR = '/Users/max/git/scitech/docs/'
-UPDATE = False
+UPDATE = True 
 QUALITY = '85'
 
 # You can edit as you wish. You need to install all of these first.
